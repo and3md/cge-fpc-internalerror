2014575b7469293f90775c3f127ed0c122460832
and3md <and3md@gmail.com>
2023-02-21 10:40:36 +0100

    Remove functions form TX3DVersion object.

--- src/x3d/castleinternalx3dlexer.pas
+++ src/x3d/castleinternalx3dlexer.pas
@@ -27,11 +27,6 @@ type
 
   TX3DVersion = object
     Major, Minor: Integer;
-    function FileExtension(const Encoding: TX3DEncoding;
-      const ForceConvertingToX3D: boolean = false): string;
-    { File filters if you want to save a file using Save3D. }
-    function FileFilters(const Encoding: TX3DEncoding;
-      const ForceConvertingToX3D: boolean = false): string;
   end;
 
   TX3DLexer = class
@@ -40,41 +35,5 @@ type
 implementation
 
 uses X3DFields;// remove this X3DFields (X3DLoadInternalUtils was here before) include to get rid of internal error
-
-
-{ TX3DVersion --------------------------------------------------------------- }
-
-function TX3DVersion.FileExtension(const Encoding: TX3DEncoding;
-  const ForceConvertingToX3D: boolean): string;
-begin
-  if Encoding = xeXML then
-    Result := '.x3d' else
-  if (Major >= 3) or ForceConvertingToX3D then
-    Result := '.x3dv' else
-    Result := '.wrl';
-end;
-
-function TX3DVersion.FileFilters(const Encoding: TX3DEncoding;
-  const ForceConvertingToX3D: boolean): string;
-const
-  SaveVRMLClassic_FileFilters =
-  'All files|*|' +
-  '*VRML (*.wrl)|*.wrl|' +
-  'VRML (compressed) (*.wrz, *.wrl.gz)|*.wrz;*.wrl.gz';
-  SaveX3DClassic_FileFilters =
-  'All files|*|' +
-  '*X3D classic (*.x3dv)|*.x3dv|' +
-  'X3D classic (compressed) (*.x3dvz, *.x3dv.gz)|*.x3dvz;*.x3dv.gz';
-  SaveX3DXml_FileFilters =
-  'All files|*|' +
-  '*X3D XML (*.x3d)|*.x3d|' +
-  'X3D XML (compressed) (*.x3dz, *.x3d.gz)|*.x3dz;*.x3d.gz';
-begin
-  if Encoding = xeXML then
-    Result := SaveX3DXml_FileFilters else
-  if (Major >= 3) or ForceConvertingToX3D then
-    Result := SaveX3DClassic_FileFilters else
-    Result := SaveVRMLClassic_FileFilters;
-end;
 
 end.
