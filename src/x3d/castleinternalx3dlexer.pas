247c89b444bd5cce48c7f7083c1f841104d7208d
and3md <and3md@gmail.com>
2023-02-21 10:13:01 +0100

    Removed a lot of types from CastleInternalX3DLexer.

--- src/x3d/castleinternalx3dlexer.pas
+++ src/x3d/castleinternalx3dlexer.pas
@@ -23,80 +23,6 @@ interface
 uses SysUtils, Classes, Math;
 
 type
-  { Valid keywords for all VRML / X3D versions. }
-  TX3DKeyword = (vkDEF, vkEXTERNPROTO, vkFALSE, vkIS, vkNULL, vkPROTO, vkROUTE,
-    vkTO, vkTRUE, vkUSE, vkEventIn, vkEventOut, vkExposedField, vkField,
-    { Below keywords are X3D-only as far as specification is concerned.
-      However, we decide to support IMPORT/EXPORT for older VRML versions
-      too (the downside is that you cannot name your nodes like this,
-      but the upside is that you can use these features in all VRML versions.) } { }
-    vkAS, vkEXPORT, vkIMPORT,
-    { X3D-only keywords below } { }
-    vkCOMPONENT, vkMETA, vkPROFILE, vkUNIT,
-    vkInputOnly, vkOutputOnly, vkInputOutput, vkInitializeOnly);
-
-  TX3DKeywords = set of TX3DKeyword;
-
-const
-  VRML10Keywords = [vkDEF, vkUSE, vkFALSE, vkTRUE];
-  VRML20Keywords = [vkDEF .. vkIMPORT] +
-    { META accepted as Titania demos use it } [vkMETA];
-  X3DKeywords = [Low(TX3DKeyword) .. High(TX3DKeyword)] -
-    [vkEventIn, vkEventOut, vkExposedField, vkField];
-
-type
-  { Lexer token. }
-  TX3DToken = (
-    vtKeyword,
-    vtName,
-
-    { Symbols for all VRML versions.
-      @groupBegin }
-    vtOpenCurlyBracket, vtCloseCurlyBracket,
-    vtOpenSqBracket, vtCloseSqBracket,
-    { @groupEnd }
-
-    { Symbols below are only for VRML <= 1.0.
-      In VRML 2.0, they are no longer valid symbols
-      (comma is even considered a whitespace).
-      They will never be returned by lexer when reading VRML >= 2.0 files.
-
-      @groupBegin }
-    vtOpenBracket, vtCloseBracket, vtBar, vtComma,
-    { @groupEnd }
-
-    { Symbols below are only for VRML >= 2.0.
-      They will never be returned by lexer when reading VRML < 2.0 files.
-      @groupBegin }
-    vtPeriod,
-    { @groupEnd }
-
-    { Symbols below are only for VRML >= 3.0, that is X3D.
-      They will never be returned by lexer when reading VRML < 3.0 files.
-      @groupBegin }
-    vtColon,
-    { @groupEnd }
-
-    { Back to version-neutral tokens, suitable for all VRML / X3D versions.
-      @groupBegin }
-    vtFloat, vtInteger, vtString,
-    { @groupEnd }
-
-    { vtEnd means that we're standing at the end of stream, no more tokens.
-      From this point, further reads using NextToken from stream will
-      always result in vtEnd (they will not raise an error). }
-    vtEnd);
-  TX3DTokens = set of TX3DToken;
-
-  { Any error related to VRML/X3D. }
-  EX3DError = class(Exception);
-
-  EX3DGzipCompressed = class(EX3DError);
-
-const
-  TokenNumbers : TX3DTokens = [vtFloat, vtInteger];
-
-type
   TX3DEncoding = (xeClassic, xeXML);
 
   TX3DVersion = object
@@ -133,57 +59,10 @@ type
     destructor Destroy; override;
   end;
 
-  { Error when reading VRML/X3D classic encoding. }
-  EX3DClassicReadError = class(EX3DError)
-  public
-    { Standard constructor.
-
-      Lexer instance must be valid for this call, but not longer.
-      That is, you can free the lexer after this constructor finished,
-      it doesn't need to be valid for the whole lifetime of this object. }
-    constructor Create(Lexer: TX3DLexer; const s: string);
-  end;
-
-  { Error when reading VRML/X3D. For now, just equal to EX3DClassicReadError,
-    later may be an ancestor to EX3DClassicReadError.
-    Problems in other encodings (XML) are for now always turned into warnings. }
-  EX3DReadError = EX3DClassicReadError;
-
-  EX3DLexerError = class(EX3DClassicReadError);
-  EX3DParserError = class(EX3DClassicReadError);
-
-const
-  X3DKeywordsName: array [TX3DKeyword] of string = (
-    'DEF', 'EXTERNPROTO', 'FALSE', 'IS', 'NULL', 'PROTO', 'ROUTE',
-    'TO', 'TRUE', 'USE', 'eventIn', 'eventOut', 'exposedField', 'field',
-    'AS', 'EXPORT', 'IMPORT',
-    'COMPONENT', 'META', 'PROFILE', 'UNIT',
-    'inputOnly', 'outputOnly', 'inputOutput', 'initializeOnly'
-    );
-
 implementation
 
 uses X3DFields;// remove this X3DFields (X3DLoadInternalUtils was here before) include to get rid of internal error
 
-const
-  X3DTokenNames: array [TX3DToken] of string = (
-    'keyword', 'name',
-    '"{"', '"}"', '"["', '"]"', '"("', '")"', '"|"', '","', '"."', '":"',
-    'float', 'integer', 'string', 'end of stream');
-
-function ArrayPosX3DKeywords(const s: string; out Index: TX3DKeyword): boolean;
-var
-  I: TX3DKeyword;
-begin
-  for I := Low(X3DKeywords) to High(X3DKeywords) do
-    if X3DKeywordsName[I] = s then
-    begin
-      Index := I;
-      Result := true;
-      Exit;
-    end;
-  Result := false;
-end;
 
 { TX3DVersion --------------------------------------------------------------- }
 
@@ -229,14 +108,6 @@ end;
 destructor TX3DLexer.Destroy;
 begin
   inherited;
-end;
-
-{ Exceptions ----------------------------------------------------------------- }
-
-constructor EX3DClassicReadError.Create(Lexer: TX3DLexer; const s: string);
-begin
-  inherited Create(Format('Error at line %d column %d: ',
-    [111, 1111]) + S);
 end;
 
 end.
