c7f0c3d9c1514abf4105dd9f5db8ee70afbbdb5a
and3md <and3md@gmail.com>
2023-02-21 10:17:13 +0100

    Remove TX3DLexer constructor and destructor.

--- src/x3d/castleinternalx3dlexer.pas
+++ src/x3d/castleinternalx3dlexer.pas
@@ -34,29 +34,7 @@ type
       const ForceConvertingToX3D: boolean = false): string;
   end;
 
-  { VRML/X3D (classic encoding) lexer.
-
-    The lexer always "looks" (i.e. contains in Token and TokenXxx fields)
-    at the next not yet interpreted token.
-
-    Remember that VRML is case-sensitive, so TokenName and TokenString
-    should be compared in case-sensitive manner. Also note that
-    for VRML/X3D >= 2.0 these fields contain UTF-8 encoded strings.
-
-    Note that this lexer can read only from @link(TPeekCharStream), not just
-    from any TStream. You may have to wrap your stream in some
-    @link(TPeekCharStream) descendant (for example create TFileStream
-    and then wrap it inside @link(TBufferedReadStream)). }
   TX3DLexer = class
-  public
-    { Standard constructor.
-      After constructor call, @link(Version) is already set,
-      it's checked that file is not compressed by gzip, and the first
-      Token is already read.
-      @raises(EX3DGzipCompressed If the Stream starts with gzip file header.) }
-    constructor Create(AOwnsStream: boolean);
-
-    destructor Destroy; override;
   end;
 
 implementation
@@ -97,17 +75,6 @@ begin
   if (Major >= 3) or ForceConvertingToX3D then
     Result := SaveX3DClassic_FileFilters else
     Result := SaveVRMLClassic_FileFilters;
-end;
-
-{ TX3DLexer ------------------------------------------------------------- }
-
-constructor TX3DLexer.Create(AOwnsStream: boolean);
-begin
-end;
-
-destructor TX3DLexer.Destroy;
-begin
-  inherited;
 end;
 
 end.
